1721039cd49e0282773f411f352db9300bd71e6d
Eric Lund <eric.lund@hpe.com>
2025-05-19 16:44:32 -0500

    Add simple deployment mechanism to application

--- vtds_application_openchami/private/application.py
+++ vtds_application_openchami/private/application.py
@@ -25,9 +25,11 @@
 """
 
 from vtds_base import (
+    info_msg,
     ContextualError,
 )
 from vtds_base.layers.application import ApplicationAPI
+from . import DEPLOY_FILES
 
 
 class Application(ApplicationAPI):
@@ -51,6 +53,34 @@ class Application(ApplicationAPI):
         self.build_dir = build_dir
         self.prepared = False
 
+    @staticmethod
+    def __deploy_files(connections, files):
+        """Copy files to the blades or nodes connected in
+        'connections' based on the manifest and run the appropriate
+        deployment script(s).
+
+        """
+        for source, dest, mode, tag, run in files:
+            info_msg(
+                "copying '%s' to host-node node(s) '%s'" % (
+                    source, dest
+                )
+            )
+            connections.copy_to(
+                source, dest,
+                recurse=False, logname="upload-application-%s-to-%s" % (
+                    tag, 'host-node'
+                )
+            )
+            cmd = "chmod %s %s;" % (mode, dest)
+            info_msg(
+                "chmod'ing '%s' to %s on host-node node(s)" % (dest, mode)
+            )
+            connections.run_command(cmd, "chmod-file-%s-on" % tag)
+            if run:
+                info_msg("running '%s' on host-node node(s)" % dest)
+                connections.run_command(dest, "run-%s-on" % tag)
+
     def consolidate(self):
         return
 
@@ -71,7 +101,9 @@ class Application(ApplicationAPI):
             raise ContextualError(
                 "cannot deploy an unprepared application, call prepare() first"
             )
-        print("Deploying vtds-application-openchami")
+        virtual_nodes = self.stack.get_cluster_api().get_virtual_nodes()
+        with virtual_nodes.ssh_connect_nodes(['host_node']) as connections:
+            self.__deploy_files(connections, DEPLOY_FILES)
 
     def remove(self):
         if not self.prepared:
